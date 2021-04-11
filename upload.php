2816fc8b5eb7b4bdaf0c63020f3095c14ac1d1b0
VinayKishore901 <vkishore_be18@thapar.edu>
2021-04-11 13:59:10 +0530

    first commit

---
+++ upload.php
@@ -0,0 +1,15 @@
+ <?php
+           if(isset($_POST['upload'])){
+           echo"pressed";
+
+           $file_name= $_FILES['file']['name'];
+           $file_type= $_FILES['file']['type'];
+           $file_name= $_FILES['file']['size'];
+           $file_tem_loc=$_FILES['file']['tmp_name'];
+           $file_store="uploads/".$file_name;
+
+           move_uploaded_file($file_tem_loc,$file_store);
+            
+
+       }     
+?>
